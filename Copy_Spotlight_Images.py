741360a2f95181801cf2f6a965ff80fdd956f3f2
Corey Buckley <coreybuckley13@gmail.com>
2018-08-20 22:32:12 -0400

    Spotlight tool

---
+++ Copy_Spotlight_Images.py
@@ -0,0 +1,51 @@
+#81.05 ms on average of 100 trials (calculated by powershell)
+#About 2ms when the main action of this script is run and all the setup is done (after line 45)
+import os, time, shutil
+import shelve #used as a way for persistent storage. Creates a .dat file that acts like a dictionary
+from time import strftime #format a string with time specific formatters
+begin = time.perf_counter()
+
+userpath = os.environ.get("USERPROFILE") #C:/Users/[person]
+
+windowsSpotlightPath = userpath + r"\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\\"
+destPath = userpath + r"\Desktop\Spotlight_Images\Test\\"
+os.chdir(destPath)
+spotlightImages = []
+biggerThan = 1024*200 #bigger than 200KB
+today = strftime("%m%d%Y",time.localtime()) #month,day,year
+
+
+fileSize = lambda file: os.path.getsize(windowsSpotlightPath + file)
+files = os.listdir(windowsSpotlightPath)
+
+files.sort(reverse=True,key=fileSize)
+
+def isSpotlightImage(file):
+    absPath = windowsSpotlightPath + file
+    metadata = os.stat(absPath)
+    isBigger = metadata.st_size > biggerThan
+    if (isBigger):
+        return True
+    else:
+        return False
+
+i = 0
+while(i < len(files) and isSpotlightImage(files[i])): #short-circuit so we don't get an index out of bounds (if all files were valid spotlight images)
+    spotlightImages.append(files[i])
+    i += 1
+del files
+
+countFile = shelve.open("count")
+count = 0
+
+try:
+	count = countFile["count"]
+except KeyError:
+	countFile["count"] = 0
+
+for img in spotlightImages:
+	count += 1
+	newName = today + "_" + str(count)
+	shutil.copy2(windowsSpotlightPath + img, destPath + newName + ".png")
+
+countFile["count"] = count
---
+++ README.md
@@ -0,0 +1,5 @@
+This python script uses the shelve module to store a simple incrementing count for each file you copy from the Windows spotlight folder. This count will be appended onto the new name of the image. The naming of the file follows this format:
+
+[date]_[imageNumber]
+
+Run this script to create a folder on your desktop titled "Spotlight_Images" that will store the images currently being used for the Windows Spotlight feature.
